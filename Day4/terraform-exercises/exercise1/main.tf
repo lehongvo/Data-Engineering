bc21484550f76e8372c6e8f963216dd81960c939
vincentVo <lehongvi19x@gmail.com>
2025-04-25 17:24:45 +0700

    [Vincent][terraform][Done: Ex1]

--- Day4/terraform-exercises/exercise1/main.tf
+++ Day4/terraform-exercises/exercise1/main.tf
@@ -19,13 +19,13 @@ resource "google_storage_bucket" "raw_data" {
   force_destroy = true
 
   uniform_bucket_level_access = true
-  
+
   lifecycle_rule {
     condition {
-      age = 90  # Chuyển data sau 90 ngày
+      age = 90 # Chuyển data sau 90 ngày
     }
     action {
-      type = "SetStorageClass"
+      type          = "SetStorageClass"
       storage_class = "COLDLINE"
     }
   }
@@ -37,15 +37,63 @@ resource "google_bigquery_dataset" "analytics" {
   description = "Dataset for raw data analysis"
   location    = var.region
 
-  default_table_expiration_ms = 7776000000  # 90 days
+  default_table_expiration_ms = 7776000000 # 90 days
 
   access {
     role          = "OWNER"
     special_group = "projectOwners"
   }
-  
+
   access {
     role          = "READER"
     special_group = "projectReaders"
   }
+}
+
+# Tạo VPC Network
+resource "google_compute_network" "vpc_network" {
+  name                    = "data-processing-vpc"
+  auto_create_subnetworks = false
+}
+
+# Tạo Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "data-processing-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  network       = google_compute_network.vpc_network.id
+  region        = var.region
+}
+
+# Tạo Compute Engine Instance
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.subnet.name
+
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+
+  tags = var.instance_tags
+
+  metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  metadata_startup_script = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y python3-pip
+              EOF
 }
